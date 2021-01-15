1c84ab253ee10ea27da49fa996164d33a88c5203
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2021-01-15 22:28:29 +0900

    AutoSequence renamed and correct the topes

--- demos/AutoSequence.py
+++ demos/AutoSequence.py
@@ -1,13 +1,15 @@
+import os
 from smartdoor import SmartDoor
 from logging import config, getLogger
 
+DIR = os.path.dirname(__file__)
 
 # Set logger
-config.fileConfig("logging.conf")
+config.fileConfig(os.path.join(DIR, "logging.conf"))
 logger = getLogger()
 
 # Define path to conf.yaml
-path = "conf.yaml"
+path = os.path.join(DIR, "conf.yaml")
 
 # Instantiate
 door = SmartDoor(path=path, log=logger)
@@ -25,9 +27,9 @@ try:
 
         # If buttom is pushed
         if tag is None:
-            door.PWM_switch_LED.ChangeDutyCycle(50)
-            door.door_seaquence(user="スイッチ操作者")
-            door.PWM_switch_LED.ChangeDutyCycle(100)
+            door.PWM_LED_switch.ChangeDutyCycle(50)
+            door.door_sequence(user="スイッチ操作者")
+            door.PWM_LED_switch.ChangeDutyCycle(100)
             continue
 
         # If IC card is detected
@@ -42,7 +44,7 @@ try:
 
             # If authentication completed
             else:
-                door.door_seaquence(user=user)
+                door.door_sequence(user=user)
 
         # If keyInterrupt (tag == False)
         else:
